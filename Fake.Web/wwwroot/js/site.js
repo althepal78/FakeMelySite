3413462690ff6be54dd8fa939eba38f21b141b63
Albert Cardona II <bigdaddycardona@live.com>
2022-11-22 12:56:31 -0500

    a lot of changes to cards and stuff like that

--- Fake.Web/wwwroot/js/site.js
+++ Fake.Web/wwwroot/js/site.js
@@ -1,4 +1,41 @@
-﻿const dropBtn = document.querySelector("#drop-btn");
-dropBtn.addEventListener('click', () => {
-    document.querySelector('.components').classList.toggle('show');
-})
+﻿// const activePage = window.location.pathname;
+// const navLinks = document.querySelectorAll('nav a')
+// .forEach(link =>{
+//     if(link.href.includes(`${activePage}`)){
+//         link.classList.add('active');
+//         link.classList.add('link')
+//     }
+// });
+
+document.addEventListener("click", (e) => {
+    const isDropdownButton = e.target.matches("[data-nav-dropdown-button]");
+
+    if (!isDropdownButton && e.target.closest("[data-nav-dropdown]") != null)
+        return;
+
+    let currentDropdown;
+    if (isDropdownButton) {
+        currentDropdown = e.target.closest("[data-nav-dropdown]");
+        // console.log(currentDropdown)
+        currentDropdown.classList.toggle("active");
+        console.log("is it clikcing");
+    }
+
+    document
+        .querySelectorAll("[data-nav-dropdown].active")
+        .forEach((dropdown) => {
+            if (dropdown === currentDropdown) return;
+            dropdown.classList.remove("active");
+        });
+});
+
+document.querySelector("#cards").onmousemove = e => {
+    for (const card of document.querySelectorAll(".card")) {
+        const rect = card.getBoundingClientRect(),
+            x = e.clientX - rect.left,
+            y = e.clientY - rect.top;
+
+        card.style.setProperty("--mouse-x", `${x}px`);
+        card.style.setProperty("--mouse-y", `${y}px`);
+    };
+}
